55f587446f4344f997ee133a975c1f1baf05e263
Thiagox789 <almadathiagoignacio@gmail.com>
2025-06-13 17:00:47 -0300

    ANDA BIEN AHORA

--- Leer.py
+++ Leer.py
@@ -1,18 +1,25 @@
-import serial 
+import serial
 import time
-
+import json
 
 arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-time.sleep(2)  
+time.sleep(2)
 
 try:
     while True:
-        line = arduino.readline().decode('utf-8').strip()  # Leer línea y decodificar
+        line = arduino.readline().decode('utf-8').strip()
         if line:
             partes = line.split(',')
             if len(partes) == 2:
                 temperatura, humedad = partes
-                print(f"Temperatura: {temperatura} °C | Humedad: {humedad} %")
+                datos = {
+                    "temperatura": float(temperatura),
+                    "humedad": float(humedad),
+                    "timestamp": time.strftime("%Y-%m-%d %H:%M:%S")
+                }
+                with open("data.json", "w") as archivo:
+                    json.dump(datos, archivo)
+                print(f"Guardado: {datos}")
             else:
                 print("Dato inválido:", line)
 except KeyboardInterrupt:
